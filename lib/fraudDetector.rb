86057d440dfe7b1765a01d5642862cde57dc2f5e
greta-ramaneckaite <gramaneckaite1@sheffield.ac.uk>
2018-06-25 15:58:08 +0100

    initial commit

---
+++ lib/fraudDetector.rb
@@ -0,0 +1,53 @@
+require 'date'
+
+class FraudDetector
+	attr_reader :postcode, :card_number, :card_expiry
+
+	def initialise(postcode, card_number, card_expiry)
+		@user = User.New(:address => "S10 2FL", credit_card => "4111 1111 1111 1111")
+		@address = Address.new(:user_id => "1", :postcode => "S10 2FL")
+		@credit_card = Credit_card.new(:last_four_digits => "1111", :expiry_month => "06", :expiry_year => "2018")
+	end
+
+	def fraudulent?
+		# Separate booleans for each check of fraud
+		postcode_bool = false
+		card_fraud_bool = false
+		card_number_bool = false
+		card_expiry_bool = false
+
+		# Check postcode ( + manage spaces)
+		user_postcode = postcode.delete(' ')
+		if Address.where(postcode: user_postcode)
+			postcode_bool = true
+		end
+
+		# card_number check only last 4 digits
+		cn = cn.delete(' ')
+		cn = card_number[-4..-1]
+		if Credit_card.where(last_four_digits: cn)
+			card_number_bool = true
+		end
+
+		# card_expiry check correctly month and year match
+		card_expiry_date = card_expiry.split('/')
+		user_date = Date.new(card_expiry_date.first, card_expiry_date.last)
+		db_date = Date.new(Card_expiry.expiry_year, Card_expiry.expiry_month)
+
+		if user_date == db_date
+			card_expiry_bool = true
+		end
+
+		# Card fraudulence
+		if card_number_bool == true and card_expiry_bool == true
+			card_fraud_bool = true
+		end
+
+		# Final check: if card and address frauds are both true
+		if postcode_bool == true and card_fraud_bool == true
+			return true
+		else
+			return false
+		end		
+	end
+end
---
+++ lib/fraudDetector/version.rb
@@ -0,0 +1,3 @@
+module FraudDetector
+  VERSION = "0.1.0"
+end
